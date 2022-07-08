000334c4fcd1d0835ff50a9e6943fb3a807385b4
RUPAM_PC\rupam <rdebmail@gmail.com>
2022-07-08 13:40:59 +0530

    Added Run controls with LeftShift with speed and animation

--- Race3D/Assets/Scripts/PlayerController.cs
+++ Race3D/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed;
     public float rotationSpeed = 50.0f;
     public Animator tigerAnimator;
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        float translation = Input.GetAxisRaw("Vertical") * speed;
+        float rotation = Input.GetAxisRaw("Horizontal") * rotationSpeed;
 
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
@@ -18,12 +18,29 @@ public class PlayerController : MonoBehaviour
         transform.Translate(0, 0, translation);
 
         transform.Rotate(0, rotation, 0);
-        if(translation == 0f)
+        if (translation > 0f && Input.GetKey(KeyCode.LeftShift))
         {
+            tigerAnimator.SetBool("Run", true);
             tigerAnimator.SetBool("Walk", false);
+            speed = 5f;
+        }
+        else if (translation > 0f && Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            tigerAnimator.SetBool("Walk", true);
+            tigerAnimator.SetBool("Run", false);
+            speed = 2f;
+        }
+        else if (translation > 0f)
+        {
+            tigerAnimator.SetBool("Walk", true);
+            tigerAnimator.SetBool("Run", false);
+            speed = 2f;
         }
         else
-            tigerAnimator.SetBool("Walk", true);
+        {
+            tigerAnimator.SetBool("Walk", false);
+            tigerAnimator.SetBool("Run", false);
+        }
 
     }
 }
