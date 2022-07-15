ddb61936b2d2b5912ed2da6cb86a16287a408f25
RUPAM_PC\rupam <rdebmail@gmail.com>
2022-07-15 16:03:04 +0530

    Added sound for spear

--- Race3D/Assets/Scripts/AttackingScripts/SpearController.cs
+++ Race3D/Assets/Scripts/AttackingScripts/SpearController.cs
@@ -6,20 +6,23 @@ public class SpearController : MonoBehaviour
 {
     public Rigidbody spearBody;
     public Transform shootTransform;
-
+    public GameObject tigerObject;
     private void Awake()
     {
+        tigerObject = GameObject.FindGameObjectWithTag("Player");
         shootTransform = GetComponentInParent<Transform>();
     }
     void Start()
     {
         spearBody.velocity = 20f * shootTransform.forward;
+        gameObject.GetComponent<AudioSource>().Play();
     }
 
     void Update()
     {
         transform.up = Vector3.Slerp(transform.up, spearBody.velocity.normalized, Time.deltaTime * 15);
     }
+
     private void OnCollisionEnter(Collision target)
     {
         if (target.gameObject.tag == "Enemy")
